fc5faa69a27dd67a5e98e03067cc4e5e0abab599
sian <chicori3@naver.com>
2023-06-12 16:23:46 +0900

    Initial Commit

---
+++ src/main/java/com/flab/infrun/InfrunApplication.java
@@ -0,0 +1,12 @@
+package com.flab.infrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfrunApplication.class, args);
+    }
+}
