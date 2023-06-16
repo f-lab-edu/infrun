7cbc464572596247454572dd6c6edf57294567bb
sian <chicori3@naver.com>
2023-06-17 02:05:07 +0900

    feat(#2) - 공통 에러 코드 추가, 응답 객체 수정

--- src/main/java/com/flab/infrun/common/exception/ErrorCode.java
+++ src/main/java/com/flab/infrun/common/exception/ErrorCode.java
@@ -2,9 +2,13 @@ package com.flab.infrun.common.exception;
 
 public enum ErrorCode {
 
-    SYSTEM_ERROR("시스템 에러가 발생하였습니다.");
+    SYSTEM_ERROR("시스템 에러가 발생하였습니다."),
+    INVALID_PARAMETER("요청값이 올바르지 않습니다."),
 
-    public final String message;
+    // MEMBER
+    DUPLICATED_EMAIL("이미 존재하는 이메일입니다.");
+
+    private final String message;
 
     ErrorCode(final String message) {
         this.message = message;
--- src/main/java/com/flab/infrun/common/response/Response.java
+++ src/main/java/com/flab/infrun/common/response/Response.java
@@ -7,16 +7,22 @@ import com.flab.infrun.common.exception.ErrorCode;
 
 public record Response<T>(
     boolean success,
+    @JsonInclude(Include.NON_NULL)
     T data,
     @JsonInclude(Include.NON_NULL)
-    ErrorCode errorCode
-) {
+    ErrorCode errorCode,
+    @JsonInclude(Include.NON_NULL)
+    String message) {
 
     public static <T> Response<T> success(final T data) {
-        return new Response<>(true, data, null);
+        return new Response<>(true, data, null, null);
     }
 
-    public static <T> Response<T> fail(final T data, final ErrorCode errorCode) {
-        return new Response<>(false, data, errorCode);
+    public static <T> Response<T> fail(final ErrorCode errorCode) {
+        return new Response<>(false, null, errorCode, null);
+    }
+
+    public static <T> Response<T> fail(final ErrorCode errorCode, final String message) {
+        return new Response<>(false, null, errorCode, message);
     }
 }
