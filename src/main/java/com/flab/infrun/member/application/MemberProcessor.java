ca614fd98a726facefc1487af89f5edc39f8bb64
sian <chicori3@naver.com>
2023-06-19 15:06:50 +0900

    feat(#2) - 회원 가입 시 설정한 PasswordEncoder 사용

--- src/main/java/com/flab/infrun/member/application/MemberProcessor.java
+++ src/main/java/com/flab/infrun/member/application/MemberProcessor.java
@@ -5,30 +5,33 @@ import com.flab.infrun.member.domain.Member;
 import com.flab.infrun.member.domain.MemberRepository;
 import com.flab.infrun.member.domain.exception.DuplicatedEmailException;
 import com.flab.infrun.member.domain.exception.DuplicatedNicknameException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MemberProcessor {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public MemberProcessor(final MemberRepository port) {
+    public MemberProcessor(final MemberRepository port, final PasswordEncoder passwordEncoder) {
         this.memberRepository = port;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Long register(SignupCommand command) {
-        registerValidate(command);
+        validateRegisterMember(command);
 
         Member member = memberRepository.save(
             Member.of(
                 command.nickname(),
                 command.email(),
-                command.password()));
+                passwordEncoder.encode(command.password())));
 
         return member.getId();
     }
 
-    private void registerValidate(final SignupCommand command) {
+    private void validateRegisterMember(final SignupCommand command) {
         if (memberRepository.existsByEmail(command.email())) {
             throw new DuplicatedEmailException();
         }
