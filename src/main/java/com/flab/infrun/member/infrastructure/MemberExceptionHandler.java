32e83cfe06c19abc43bd38b7dc572119cc76e7bc
sian <chicori3@naver.com>
2023-06-21 19:18:22 +0900

    refactor(#2) - ExceptionHandler에 멤버 도메인 예외 명시

--- src/main/java/com/flab/infrun/member/infrastructure/MemberExceptionHandler.java
+++ src/main/java/com/flab/infrun/member/infrastructure/MemberExceptionHandler.java
@@ -3,6 +3,8 @@ package com.flab.infrun.member.infrastructure;
 import com.flab.infrun.common.exception.ErrorCode;
 import com.flab.infrun.common.exception.SystemException;
 import com.flab.infrun.common.response.Response;
+import com.flab.infrun.member.domain.exception.DuplicatedEmailException;
+import com.flab.infrun.member.domain.exception.DuplicatedNicknameException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +15,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class MemberExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler({
+        DuplicatedNicknameException.class,
+        DuplicatedEmailException.class
+    })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Response<ErrorCode> handleException(SystemException e) {
         return Response.fail(e.getErrorCode());
