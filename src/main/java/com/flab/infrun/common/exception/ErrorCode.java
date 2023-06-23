208ee90fb782550b5fe70ec5c354a2fe76068e14
sian <chicori3@naver.com>
2023-06-23 18:02:12 +0900

    feat(#13) - INVALID_AUTHENTICATION 에러 코드 추가

--- src/main/java/com/flab/infrun/common/exception/ErrorCode.java
+++ src/main/java/com/flab/infrun/common/exception/ErrorCode.java
@@ -2,12 +2,14 @@ package com.flab.infrun.common.exception;
 
 public enum ErrorCode {
 
+    // COMMON
     SYSTEM_ERROR("시스템 에러가 발생하였습니다."),
     INVALID_PARAMETER("요청값이 올바르지 않습니다."),
 
     // MEMBER
     DUPLICATED_NICKNAME("이미 존재하는 닉네임입니다."),
-    DUPLICATED_EMAIL("이미 존재하는 이메일입니다.");
+    DUPLICATED_EMAIL("이미 존재하는 이메일입니다."),
+    INVALID_AUTHENTICATION("유효하지 않은 인증정보입니다.");
 
     private final String message;
 
