9cd52802c164ec5c73a598d073e4413bc65bc94f
sian <chicori3@naver.com>
2023-06-23 14:27:08 +0900

    refactor(#2) - FieldError Null Check 코드 추가

--- src/main/java/com/flab/infrun/common/CommonExceptionHandler.java
+++ src/main/java/com/flab/infrun/common/CommonExceptionHandler.java
@@ -2,6 +2,7 @@ package com.flab.infrun.common;
 
 import com.flab.infrun.common.exception.ErrorCode;
 import com.flab.infrun.common.response.Response;
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -19,6 +20,10 @@ public class CommonExceptionHandler {
     public Response<ErrorCode> handleMethodArgumentNotValidException(
         MethodArgumentNotValidException e) {
         FieldError fieldError = e.getBindingResult().getFieldError();
+
+        if (Objects.isNull(fieldError)) {
+            return Response.fail(ErrorCode.INVALID_PARAMETER);
+        }
 
         final String sb = "Request Error"
             + " " + fieldError.getField()
