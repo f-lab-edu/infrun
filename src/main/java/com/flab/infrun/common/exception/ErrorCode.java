49fa44986cbbccef5ef5b81a96dbe012949b61c5
sian <chicori3@naver.com>
2023-06-25 02:13:36 +0900

    feat(#13) - 유저가 존재하지 않는 예외 추가

--- src/main/java/com/flab/infrun/common/exception/ErrorCode.java
+++ src/main/java/com/flab/infrun/common/exception/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
     // MEMBER
     DUPLICATED_NICKNAME("이미 존재하는 닉네임입니다."),
     DUPLICATED_EMAIL("이미 존재하는 이메일입니다."),
-    INVALID_AUTHENTICATION("유효하지 않은 인증정보입니다.");
+    INVALID_AUTHENTICATION("유효하지 않은 인증정보입니다."),
+    NOT_FOUND_MEMBER("존재하지 않는 회원입니다.");
 
     private final String message;
 
