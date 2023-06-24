c09c78d76fe4ef0ca839c99f50cd01a3676c81ca
sian <chicori3@naver.com>
2023-06-25 02:20:26 +0900

    feat(#13) - 로그인 기능 구현 및 테스트 작성

--- src/main/java/com/flab/infrun/member/application/MemberProcessor.java
+++ src/main/java/com/flab/infrun/member/application/MemberProcessor.java
@@ -1,10 +1,13 @@
 package com.flab.infrun.member.application;
 
+import com.flab.infrun.member.application.command.LoginCommand;
 import com.flab.infrun.member.application.command.SignupCommand;
 import com.flab.infrun.member.domain.Member;
 import com.flab.infrun.member.domain.MemberRepository;
 import com.flab.infrun.member.domain.exception.DuplicatedEmailException;
 import com.flab.infrun.member.domain.exception.DuplicatedNicknameException;
+import com.flab.infrun.member.domain.exception.NotFoundMemberException;
+import com.flab.infrun.member.infrastructure.session.SessionStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -16,6 +19,7 @@ public class MemberProcessor {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final SessionStorage sessionStorage;
 
     @Transactional
     public Long register(final SignupCommand command) {
@@ -26,6 +30,16 @@ public class MemberProcessor {
                 passwordEncoder.encode(command.password())));
 
         return member.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public String login(final LoginCommand command) {
+        final Member member = memberRepository.findByEmailAndPassword(command.email(),
+                command.password())
+            .orElseThrow(NotFoundMemberException::new);
+
+        return sessionStorage.store(member.getEmail(), member.getPassword())
+            .getToken();
     }
 
     private void validateRegisterMember(final SignupCommand command) {
--- src/test/java/com/flab/infrun/member/application/MemberProcessorTest.java
+++ src/test/java/com/flab/infrun/member/application/MemberProcessorTest.java
@@ -1,41 +1,45 @@
 package com.flab.infrun.member.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.flab.infrun.member.application.command.LoginCommand;
 import com.flab.infrun.member.application.command.SignupCommand;
 import com.flab.infrun.member.domain.Member;
 import com.flab.infrun.member.domain.MemberRepository;
 import com.flab.infrun.member.domain.exception.DuplicatedEmailException;
 import com.flab.infrun.member.domain.exception.DuplicatedNicknameException;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
+import com.flab.infrun.member.infrastructure.session.SessionStorage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 final class MemberProcessorTest {
 
-    private MemberRepository repository;
     private MemberProcessor processor;
+
+    private static void setUpMember(final MemberRepository repository) {
+        final String nickname = "userA";
+        final String email = "test@test.com";
+        final String password = "password";
+        repository.save(Member.of(nickname, email, password));
+    }
 
     @BeforeEach
     void setUp() {
-        repository = new StubMemberRepository();
-        processor = new MemberProcessor(repository, new FakePasswordEncoder());
+        final MemberRepository repository = new StubMemberRepository();
+        processor = new MemberProcessor(
+            repository,
+            new FakePasswordEncoder(),
+            new SessionStorage());
+
+        setUpMember(repository);
     }
 
     @Test
     @DisplayName("회원가입 시 이메일이 중복인 경우 예외가 발생한다")
     void register_duplicateEmailException() {
-        String nickname = "userA";
-        String email = "test@test.com";
-        String password = "password";
-
-        repository.save(Member.of(nickname, email, password));
-
-        SignupCommand command = new SignupCommand("userB", "test@test.com", "password");
+        final SignupCommand command = new SignupCommand("userB", "test@test.com", "password");
 
         assertThatThrownBy(() -> processor.register(command))
             .isInstanceOf(DuplicatedEmailException.class);
@@ -44,52 +48,20 @@ final class MemberProcessorTest {
     @Test
     @DisplayName("회원가입 시 닉네임이 중복인 경우 예외가 발생한다")
     void register_duplicateNicknameException() {
-        String nickname = "nickname";
-        String email = "userA@test.com";
-        String password = "password";
-
-        repository.save(Member.of(nickname, email, password));
-
-        SignupCommand command = new SignupCommand("nickname", "userB@test.com", "password");
+        final SignupCommand command = new SignupCommand("nickname", "userB@test.com", "password");
 
         assertThatThrownBy(() -> processor.register(command))
             .isInstanceOf(DuplicatedNicknameException.class);
     }
 
-    private static final class StubMemberRepository implements MemberRepository {
+    @Test
+    @DisplayName("이메일과 비밀번호가 일치하면 토큰을 반환한다")
+    void loginSuccess_returnToken() {
+        final LoginCommand command = new LoginCommand("test@test.com", "password");
 
-        private final Map<Long, Member> persistence = new ConcurrentHashMap<>();
-        private Long sequence = 0L;
+        final String token = processor.login(command);
 
-        @Override
-        public Member save(final Member member) {
-            persistence.put(++sequence, member);
-            return member;
-        }
-
-        @Override
-        public boolean existsByEmail(final String email) {
-            return persistence.values().stream()
-                .anyMatch(member -> member.getEmail().equals(email));
-        }
-
-        @Override
-        public boolean existsByNickname(final String nickname) {
-            return persistence.values().stream()
-                .anyMatch(member -> member.getNickname().equals(nickname));
-        }
+        assertThat(token).isNotNull();
     }
 
-    private static class FakePasswordEncoder implements PasswordEncoder {
-
-        @Override
-        public String encode(final CharSequence rawPassword) {
-            return rawPassword.toString();
-        }
-
-        @Override
-        public boolean matches(final CharSequence rawPassword, final String encodedPassword) {
-            return Objects.equals(rawPassword.toString(), encodedPassword);
-        }
-    }
 }
