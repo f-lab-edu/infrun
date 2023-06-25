94de0ecf59e8498ef18c662cd5c809deffd80954
sian <chicori3@naver.com>
2023-06-25 19:44:34 +0900

    test(#13) - 회원 로그인 API 테스트 작성

--- src/test/java/com/flab/infrun/member/presentation/MemberControllerTest.java
+++ src/test/java/com/flab/infrun/member/presentation/MemberControllerTest.java
@@ -16,17 +16,21 @@ import org.springframework.http.MediaType;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 final class MemberControllerTest {
 
-    private static final String REGISTER_MEMBER_URI = "/members";
+    private static final String MEMBER_URI = "/members";
 
     @LocalServerPort
     private int port;
 
-    public static SignupRequest createCorrectRequest() {
+    public static SignupRequest createCorrectSignupRequest() {
         return new SignupRequest("nickname", "test@test.com", "1234Qwer!");
     }
 
-    public static SignupRequest createIncorrectRequest() {
+    public static SignupRequest createIncorrectSignupRequest() {
         return new SignupRequest("", "", "");
+    }
+
+    public static LoginRequest createCorrectLoginRequest() {
+        return new LoginRequest("test@test.com", "1234Qwer!");
     }
 
     private static ExtractableResponse<Response> signup(final SignupRequest request) {
@@ -35,7 +39,7 @@ final class MemberControllerTest {
             .all()
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .body(request)
-            .post(REGISTER_MEMBER_URI)
+            .post(MEMBER_URI)
             .then()
             .log()
             .all()
@@ -52,7 +56,7 @@ final class MemberControllerTest {
     @Test
     @DisplayName("회원가입이 성공하면 201 상태코드와 유저 ID를 반환한다")
     void register_success() {
-        var response = signup(createCorrectRequest());
+        var response = signup(createCorrectSignupRequest());
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(response.jsonPath().getLong("data")).isNotNull();
@@ -61,7 +65,7 @@ final class MemberControllerTest {
     @Test
     @DisplayName("회원가입 시 입력값이 잘못되면 400 상태코드와 에러 메시지를 반환한다")
     void register_incorrect_request() {
-        var response = signup(createIncorrectRequest());
+        var response = signup(createIncorrectSignupRequest());
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         assertThat(response.jsonPath().getString("errorCode")).isNotNull();
@@ -70,10 +74,29 @@ final class MemberControllerTest {
     @Test
     @DisplayName("회원가입 시 닉네임 또는 이메일이 중복이면 400 상태코드와 에러 메시지를 반환한다")
     void register_duplicateEmail() {
-        signup(createCorrectRequest());
-        var response = signup(createCorrectRequest());
+        signup(createCorrectSignupRequest());
+        var response = signup(createCorrectSignupRequest());
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         assertThat(response.jsonPath().getString("errorCode")).isNotNull();
+    }
+
+    @Test
+    @DisplayName("로그인 시 JWT 토큰을 반환한다")
+    void loginSuccess_returnToken() {
+        signup(createCorrectSignupRequest());
+        var response = RestAssured.given()
+            .log()
+            .all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(createCorrectLoginRequest())
+            .post(MEMBER_URI + "/login")
+            .then()
+            .log()
+            .all()
+            .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("data")).isNotNull();
     }
 }
