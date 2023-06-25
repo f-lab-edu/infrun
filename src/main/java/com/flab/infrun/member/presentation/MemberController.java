e3ad8c3ab498cfedab9762d0def6bae454069b98
sian <chicori3@naver.com>
2023-06-25 19:44:22 +0900

    feat(#13) - 회원 로그인 API 작성

--- src/main/java/com/flab/infrun/member/presentation/MemberController.java
+++ src/main/java/com/flab/infrun/member/presentation/MemberController.java
@@ -3,9 +3,11 @@ package com.flab.infrun.member.presentation;
 
 import com.flab.infrun.common.response.Response;
 import com.flab.infrun.member.application.MemberFacade;
+import com.flab.infrun.member.infrastructure.jwt.TokenProvider;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberFacade facade;
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Response<Long> signup(@RequestBody @Valid final SignupRequest request) {
         var result = facade.signup(request.toCommand());
+
+        return Response.success(result);
+    }
+
+    @PostMapping("/login")
+    public Response<String> login(@RequestBody @Valid final LoginRequest request) {
+        var result = facade.login(request.toCommand());
 
         return Response.success(result);
     }
