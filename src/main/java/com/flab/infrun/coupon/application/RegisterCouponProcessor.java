01353eed2c5dbf9b70f6d365c60e9d937cd5201b
sian <chicori3@naver.com>
2023-07-07 17:34:48 +0900

    feat(#7) - 쿠폰 등록 유스케이스 로직 작성

---
+++ src/main/java/com/flab/infrun/coupon/application/RegisterCouponProcessor.java
@@ -0,0 +1,32 @@
+package com.flab.infrun.coupon.application;
+
+import com.flab.infrun.common.exception.ErrorCode;
+import com.flab.infrun.coupon.application.command.CouponRegisterCommand;
+import com.flab.infrun.coupon.application.result.CouponRegisteredResult;
+import com.flab.infrun.coupon.domain.Coupon;
+import com.flab.infrun.coupon.domain.CouponRepository;
+import com.flab.infrun.coupon.domain.exception.NotFoundCouponException;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Component
+public class RegisterCouponProcessor {
+
+    private final CouponRepository couponRepository;
+
+    @Transactional
+    public CouponRegisteredResult registerCoupon(
+        final CouponRegisterCommand command,
+        final LocalDateTime currentTime
+    ) {
+        final Coupon coupon = couponRepository.findByCouponCode(command.couponCode())
+            .orElseThrow(() -> new NotFoundCouponException(ErrorCode.NOT_FOUND_COUPON));
+
+        coupon.register(command.member(), currentTime);
+
+        return CouponRegisteredResult.from(command.member().getEmail(), coupon);
+    }
+}
---
+++ src/test/java/com/flab/infrun/coupon/application/RegisterCouponProcessorTest.java
@@ -0,0 +1,82 @@
+package com.flab.infrun.coupon.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.flab.infrun.coupon.application.command.CouponRegisterCommand;
+import com.flab.infrun.coupon.domain.Coupon;
+import com.flab.infrun.coupon.domain.CouponRepository;
+import com.flab.infrun.coupon.domain.DiscountInfo;
+import com.flab.infrun.coupon.domain.DiscountType;
+import com.flab.infrun.coupon.domain.exception.AlreadyRegisteredCouponException;
+import com.flab.infrun.coupon.domain.exception.ExpiredCouponException;
+import com.flab.infrun.coupon.domain.exception.NotFoundCouponException;
+import com.flab.infrun.member.domain.Member;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+final class RegisterCouponProcessorTest {
+
+    private final LocalDateTime expirationAt = LocalDateTime.of(2088, 1, 1, 0, 0, 0);
+    private final LocalDateTime currentTime = LocalDateTime.of(2023, 6, 30, 0, 0, 0);
+    private final String couponCode = "coupon-code";
+    private RegisterCouponProcessor sut;
+
+    @BeforeEach
+    void setUp() {
+        final CouponRepository couponRepository = new StubCouponRepository();
+        sut = new RegisterCouponProcessor(couponRepository);
+
+        couponRepository.save(
+            Coupon.create(couponCode, DiscountInfo.of(DiscountType.FIX, 1000), expirationAt));
+    }
+
+    @Test
+    @DisplayName("쿠폰을 등록한다")
+    void registerCoupon() {
+        final Member member = Member.of("tester", "test@test.com", "1234");
+        final CouponRegisterCommand command = new CouponRegisterCommand(member, couponCode);
+
+        final var result = sut.registerCoupon(command, currentTime);
+
+        assertThat(result.ownerEmail()).isEqualTo(member.getEmail());
+        assertThat(result.discountInfo().getDiscountType()).isEqualTo(DiscountType.FIX);
+        assertThat(result.discountInfo().getDiscountValue()).isEqualTo(1000);
+        assertThat(result.expirationAt()).isEqualTo(expirationAt);
+    }
+
+    @Test
+    @DisplayName("쿠폰 등록 시 쿠폰 코드와 일치하는 쿠폰이 없으면 예외가 발생한다")
+    void registerCoupon_couponCode_isNotMatched() {
+        final Member member = Member.of("tester", "test@test.com", "1234");
+        final CouponRegisterCommand command = new CouponRegisterCommand(member, "not-found-code");
+
+        assertThatThrownBy(() -> sut.registerCoupon(command, currentTime))
+            .isInstanceOf(NotFoundCouponException.class);
+    }
+
+    @Test
+    @DisplayName("쿠폰 등록 시 현재 시간이 쿠폰 만료일을 넘겼다면 예외가 발생한다")
+    void registerCoupon_expirationAt_isBeforeCurrentTime() {
+        final Member member = Member.of("tester", "test@test.com", "1234");
+        final CouponRegisterCommand command = new CouponRegisterCommand(member, couponCode);
+
+        assertThatThrownBy(() -> sut.registerCoupon(command, expirationAt.plusDays(1)))
+            .isInstanceOf(ExpiredCouponException.class);
+    }
+
+    @Test
+    @DisplayName("쿠폰 등록 시 쿠폰이 이미 등록된 상태라면 예외가 발생한다")
+    void registerCoupon_couponStatus_isNotUnregistered() {
+        final Member member = Member.of("tester", "test@test.com", "1234");
+        final CouponRegisterCommand command = new CouponRegisterCommand(member, couponCode);
+
+        assertThatThrownBy(() -> {
+            sut.registerCoupon(command, currentTime);
+            sut.registerCoupon(command, currentTime);
+        })
+            .isInstanceOf(AlreadyRegisteredCouponException.class);
+    }
+}
