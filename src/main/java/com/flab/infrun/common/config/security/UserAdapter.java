36398c1cf6b8da972147b6a02f33e072e92b4c1f
sian <chicori3@naver.com>
2023-07-09 15:37:04 +0900

    refactor(#7) - SecurityContextHolder의 유저 정보를 받기 위한 코드 추가

---
+++ src/main/java/com/flab/infrun/common/config/security/UserAdapter.java
@@ -0,0 +1,27 @@
+package com.flab.infrun.common.config.security;
+
+import com.flab.infrun.member.domain.Member;
+import com.flab.infrun.member.domain.Role;
+import java.util.Collection;
+import java.util.Set;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class UserAdapter extends User {
+
+    private final Member member;
+
+    public UserAdapter(final Member member) {
+        super(member.getEmail(), member.getPassword(), authorities(member.getRole()));
+        this.member = member;
+    }
+
+    private static Collection<? extends GrantedAuthority> authorities(final Role role) {
+        return Set.of(new SimpleGrantedAuthority(role.getValue()));
+    }
+
+    public Member getMember() {
+        return member;
+    }
+}
