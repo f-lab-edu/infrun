c2e4468be6ce833b02fe7551a6909d266fe095da
kyeum_d <77428439+kyeumd@users.noreply.github.com>
2023-08-19 16:30:03 +0900

    [#35] Test용 repository stub 생성
    - Coupon, Lecture, Member Repository 추가

---
+++ src/test/java/com/flab/infrun/utils/persistence/StubCouponRepository.java
@@ -0,0 +1,40 @@
+package com.flab.infrun.utils.persistence;
+
+import com.flab.infrun.coupon.domain.Coupon;
+import com.flab.infrun.coupon.domain.CouponRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+class StubCouponRepository implements CouponRepository {
+
+    private final Map<Long, Coupon> persistence = new HashMap<>();
+    private Long sequence = 0L;
+
+    @Override
+    public Coupon save(final Coupon coupon) {
+        persistence.put(++sequence, coupon);
+        coupon.assignId(sequence);
+
+        return coupon;
+    }
+
+    @Override
+    public List<Coupon> saveAll(final List<Coupon> coupons) {
+        coupons.forEach(coupon -> {
+            persistence.put(++sequence, coupon);
+            coupon.assignId(sequence);
+        });
+
+        return coupons;
+    }
+
+    @Override
+    public Optional<Coupon> findByCouponCodeWithLock(final String couponCode) {
+        return persistence.values().stream()
+            .filter(coupon -> Objects.equals(coupon.getCode(), couponCode))
+            .findFirst();
+    }
+}
