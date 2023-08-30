6072856c7d1ca72e33976e9193297c610572df4a
sian <chicori3@naver.com>
2023-08-30 17:25:16 +0900

    refactor(#6) - DatabaseCleaner가 작동하도록 @Table 어노테이션 추가

--- src/main/java/com/flab/infrun/cart/domain/Cart.java
+++ src/main/java/com/flab/infrun/cart/domain/Cart.java
@@ -7,12 +7,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "carts")
 @Entity
 public class Cart {
 
