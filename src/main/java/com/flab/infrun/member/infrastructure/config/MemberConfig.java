a13fb298ac9bd06616ed9f980bd0d35533852dcf
sian <chicori3@naver.com>
2023-10-03 21:00:34 +0900

    refactor(#69) - Member 도메인 Config 작성

--- src/main/java/com/flab/infrun/member/infrastructure/config/MemberConfig.java
+++ src/main/java/com/flab/infrun/member/infrastructure/config/MemberConfig.java
@@ -1,17 +1,26 @@
 package com.flab.infrun.member.infrastructure.config;
 
+import com.flab.infrun.member.domain.MemberVerifier;
+import com.flab.infrun.member.infrastructure.MemberVerifierImpl;
 import com.flab.infrun.member.infrastructure.persistence.MemberJpaRepository;
 import com.flab.infrun.member.infrastructure.persistence.MemberRepositoryAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MemberPersistenceConfig {
+public class MemberConfig {
 
     @Bean
     public MemberRepositoryAdapter memberRepositoryAdapter(
         final MemberJpaRepository memberJpaRepository
     ) {
         return new MemberRepositoryAdapter(memberJpaRepository);
+    }
+
+    @Bean
+    public MemberVerifier memberVerifier(
+        final MemberJpaRepository memberJpaRepository
+    ) {
+        return new MemberVerifierImpl(memberJpaRepository);
     }
 }
